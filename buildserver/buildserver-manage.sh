2ff2c207f086159dfd8237e3e1cdd913b6ff866a
Michael Wellner <michael.wellner@de.ibm.com>
2015-11-05 23:15:11 +0100

    Changed GitLab configuration file during startup.

--- buildserver/buildserver-manage.sh
+++ buildserver/buildserver-manage.sh
@@ -8,6 +8,9 @@ start() {
   service postfix start
 
   # Start gitlab
+  echo "gitlab_rails['gitlab_shell_ssh_port'] = 222" >> /etc/gitlab/*.rb
+  echo "external_url 'http://`hostname`:8080'" >> /etc/gitlab/*.rb
+
   sleep 3 && gitlab-ctl reconfigure & /opt/gitlab/embedded/bin/runsvdir-start
 }
 
