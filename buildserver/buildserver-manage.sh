99601acf7d7d881a5ae3eaca07bff56e479d4345
Michael Wellner <michael.wellner@de.ibm.com>
2015-11-05 11:48:54 +0100

    update manage script to don't stop after startup.

--- buildserver/buildserver-manage.sh
+++ buildserver/buildserver-manage.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-# Start Tomcat
-/usr/local/apache-tomcat/bin/startup.sh
+start() {
+  # Start Tomcat
+  /usr/local/apache-tomcat/bin/startup.sh
+}
+
+monitor() {
+  echo "Running - stop container to exit"
+	# Loop forever by default - container must be stopped manually.
+  # Here is where you can add in conditions controlling when your container will exit - e.g. check for existence of specific processes stopping or errors beiing reported
+	while true; do
+		sleep 1
+	done
+}
+
+start
+trap stop SIGTERM SIGINT
+monitor
