00f2a25cc3c5bde1dc6ea44bacbfba1941416909
Michael Wellner <michael.wellner@de.ibm.com>
2015-11-05 22:05:33 +0100

    Working on workaround.

--- buildserver/buildserver-manage.sh
+++ buildserver/buildserver-manage.sh
@@ -8,7 +8,10 @@ start() {
   service postfix start
 
   # Start gitlab
-  timeout 20s gitlab-ctl reconfigure
+  gitlab-ctl reconfigure & sleep 40; kill $!
+  pkill -f chef-client
+  sleep 10
+  pkill -f chef-client
   # Execute workaround for freezing reconfigure process. See https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/doc/common_installation_problems/README.md#reconfigure-freezes-at-ruby_blocksupervise_redis_sleep-action-run.
   cp /opt/gitlab/embedded/cookbooks/runit/files/default/gitlab-runsvdir.conf /etc/init/
   initctl start gitlab-runsvdir
