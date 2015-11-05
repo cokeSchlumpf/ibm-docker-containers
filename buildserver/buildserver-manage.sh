c136e089d2583a97c98812445061c4d516e1b39a
Michael Wellner <michael.wellner@de.ibm.com>
2015-11-05 21:41:23 +0100

    Workaround for gitlab configuration bug.

--- buildserver/buildserver-manage.sh
+++ buildserver/buildserver-manage.sh
@@ -8,6 +8,10 @@ start() {
   service postfix start
 
   # Start gitlab
+  timeout 20s gitlab-ctl reconfigure
+  # Execute workaround for freezing reconfigure process. See https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/doc/common_installation_problems/README.md#reconfigure-freezes-at-ruby_blocksupervise_redis_sleep-action-run.
+  cp /opt/gitlab/embedded/cookbooks/runit/files/default/gitlab-runsvdir.conf /etc/init/
+  initctl start gitlab-runsvdir
   gitlab-ctl reconfigure
 }
 
@@ -15,13 +19,14 @@ monitor() {
   echo "Running - stop container to exit"
 	# Loop forever by default - container must be stopped manually.
   # Here is where you can add in conditions controlling when your container will exit - e.g. check for existence of specific processes stopping or errors beiing reported
-	while true; do
+	while [ -z "$EXIT_CONTAINER" ]; do
 		sleep 1
 	done
 }
 
 stop() {
   service postfix stop
+  export EXIT_CONTAINER=1
 }
 
 start
