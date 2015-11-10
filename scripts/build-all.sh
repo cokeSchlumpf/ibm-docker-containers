f36757b8225c8f82124b10f82a67605ad8616cfa
Michael Wellner <michael.wellner@de.ibm.com>
2015-11-10 19:48:45 +0100

    Small bugfix.

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -6,18 +6,18 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 
 # Update host if first parameter is set.
-if [ -n $1 ]; then
+if [ ! -z $1 ]; then
   ./update-host.sh $1
 fi
 
 # Build images
-cd ../base-centos     && docker build -t ibm/base-centos .  && cd -
-cd ../ibm-iib         && docker build -t ibm/iib .          && cd -
-cd ../ibm-wlp         && docker build -t ibm/wlp .          && cd -
-cd ../base-dev        && docker build -t ibm/base-dev .     && cd -
-cd ../dev             && docker build -t ibm/dev .          && cd -
-cd ../build/build-dvc && docker build -t ibm/build-dvc .    && cd -
-cd ../build           && docker build -t ibm/build .        && cd -
+cd      ../base-centos     && docker build -t ibm/base-centos .  && cd - \
+  && cd ../ibm-iib         && docker build -t ibm/iib .          && cd - \
+  && cd ../ibm-wlp         && docker build -t ibm/wlp .          && cd - \
+  && cd ../base-dev        && docker build -t ibm/base-dev .     && cd - \
+  && cd ../dev             && docker build -t ibm/dev .          && cd - \
+  && cd ../build/build-dvc && docker build -t ibm/build-dvc .    && cd - \
+  && cd ../build           && docker build -t ibm/build .        && cd -
 
 cd $CURRENTDIR
 docker images
