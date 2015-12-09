787e5c89d78f9e913fe490c1789fc49764853bf4
Michael Wellner <michael.wellner@de.ibm.com>
2015-12-09 19:44:22 +0100

    sc.

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,10 +3,10 @@
 # (c) michael.wellner@de.ibm.com 2015.
 #
 # This script builds the docker image for the Dockerfile within the given directory, may modify proxy settings for Dockerfile if HTTP_PROXY is set within environment.
-# 
+#
 # Usage:
 # docker-build.sh [ -h | --help | OPTIONS ]
-# 
+#
 # Options:
 #   -p|--project
 #     The project to be build (directory names in dockerfiles/, e.g. base-dev, ibm-iib, ...).
@@ -49,65 +49,65 @@ NO_PROXY=
 
 
 main() {
-  cd ${BASEDIR} 
+  cd ${BASEDIR}
   read_variables "$@"
   check_required
   init_defaults
-  
+
   HTTP_SERVER_RUNNING=$(./docker-exec.sh --args ps | grep "http-server" > /dev/null && echo 0 || echo 1)
   SKYDOCK_RUNNING=$(./docker-exec.sh --args ps | grep "skydock" > /dev/null && echo 0 || echo 1)
-  
+
   echo "HTTP Server running: ${HTTP_SERVER_RUNNING}"
   echo "Skydock running: ${SKYDOCK_RUNNING}"
-  
+
   NO_PROXY=${NO_PROXY}
-  
+
   if [ -z ${DOWNLOAD_HOST} ] && [ ${SKYDOCK_RUNNING} -eq 0 ] && [ ${HTTP_SERVER_RUNNING} -eq 0 ]; then
   	ENV=$(./docker-exec.sh --args inspect skydock | grep -A 1 "environment" | tail -n 1 | awk -F\" '{print $2}')
   	DOMAIN=$(./docker-exec.sh --args inspect skydns | grep -A 1 "domain" | tail -n 1 | awk -F\" '{print $2}')
-  
+
   	DOWNLOAD_HOST="http-server.${ENV}.${DOMAIN}"
-  	NO_PROXY=".${ENV}.${DOMAIN},"
-  
+  	NO_PROXY=".${ENV}.${DOMAIN},${NO_PROXY}"
+
   	echo "Determined ${DOWNLOAD_HOST} as download-host ..."
   elif [ -z ${DOWNLOAD_HOST} ] && [ ${HTTP_SERVER_RUNNING} -eq 0 ]; then
   	DOWNLOAD_HOST=`./docker-exec.sh --args inspect http-server | grep "\"IPA" | awk -F\" '{ print $4 }'`
-  	NO_PROXY="${DOWNLOAD_HOST},"
-  
+  	NO_PROXY="${DOWNLOAD_HOST},${NO_PROXY}"
+
   	echo "Determined ${DOWNLOAD_HOST} as download-host ..."
   fi
-  
+
   DOWNLOAD_BASE_URL="${DOWNLOAD_HOST}:${DOWNLOAD_PORT}"
   echo "Using ${DOWNLOAD_BASE_URL} for installation files ..."
-  
+
   if [ ! -z ${HTTP_PROXY} ]; then
   	echo "Using proxy ${HTTP_PROXY} to build ${PROJECT}/Dockerfile ..."
-  
+
   	NEED_HTTP=$(cat ../dockerfiles/${PROJECT}/Dockerfile | grep "FROM ubuntu" > /dev/null && echo 0 || echo 1)
-  
+
   	HTTP_PROXY=`echo ${HTTP_PROXY} | sed "s#http://\([\.]*\)#\1#g"`
   	HTTPS_PROXY=`echo ${HTTPS_PROXY} | sed "s#http://\([\.]*\)#\1#g"`
   	NO_PROXY=`echo "${NO_PROXY}" | sed "s# ##g" | sed "s#[[:blank:]]##g" | sed "s#[[:space:]]##g"`
-  
+
   	if [ ${NEED_HTTP} -eq 0 ]; then
   		echo "Using Proxy with http:// ..."
   		HTTP_PROXY="http://${HTTP_PROXY}"
   		HTTPS_PROXY="http://${HTTPS_PROXY}"
   	fi
-  
+
   	cat ../dockerfiles/${PROJECT}/Dockerfile | sed "s#http_proxy_disabled#http_proxy=${HTTP_PROXY}#g" > ../dockerfiles/${PROJECT}/Dockerfile.proxy
   	./sed.sh --args "s#https_proxy_disabled#https_proxy=${HTTPS_PROXY}#g" ../dockerfiles/${PROJECT}/Dockerfile.proxy
   	./sed.sh --args "s#no_proxy_disabled#no_proxy=\"${NO_PROXY}\"#g" ../dockerfiles/${PROJECT}/Dockerfile.proxy
-  
+
   	if [ ! "${DOWNLOAD_HOST}" = "" ]; then
   		./sed.sh --args "s#DOWNLOAD_BASE_URL=\"\([^\"]*\)\"#DOWNLOAD_BASE_URL=\"${DOWNLOAD_BASE_URL}\"#g" ../dockerfiles/${PROJECT}/Dockerfile.proxy
   	fi
-  
+
   	echo "Transformed Dockerfile:"
   	echo "######################################################################"
   	cat ../dockerfiles/${PROJECT}/Dockerfile.proxy
   	echo "######################################################################"
-  
+
   	./docker-exec.sh --args build -t ibm/${TAGNAME} -f ../dockerfiles/${PROJECT}/Dockerfile.proxy ../dockerfiles/${PROJECT}/
   	rm ../dockerfiles/${PROJECT}/Dockerfile.proxy
   else
@@ -116,12 +116,12 @@ main() {
   	else
   		cat ../dockerfiles/${PROJECT}/Dockerfile | sed "s#DOWNLOAD_BASE_URL=\"\([^\"]*\)\"#DOWNLOAD_BASE_URL=\"${DOWNLOAD_BASE_URL}\"#g" > ../dockerfiles/${PROJECT}/Dockerfile.tmp
   	fi
-  
+
   	echo "Transformed Dockerfile:"
   	echo "######################################################################"
   	cat ../dockerfiles/${PROJECT}/Dockerfile.tmp
   	echo "######################################################################"
-  
+
   	./docker-exec.sh --args build -t ibm/${TAGNAME} -f ../dockerfiles/${PROJECT}/Dockerfile.tmp ../dockerfiles/${PROJECT}/
   	rm ../dockerfiles/${PROJECT}/Dockerfile.tmp
   fi
@@ -219,7 +219,7 @@ show_help_and_exit() {
   echo "    Ignore proxy domains."
   echo
   sleep 3
-  
+
   cd ${CURRENTDIR}
   exit $1
 }
